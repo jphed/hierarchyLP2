fe52fd2d96f61fd773fcba585a83b3270c2a494f
Jorge Parra <13104@ulsachihuahua.edu.mx>
2023-09-14 10:38:16 -0700

    Functions added

--- programHierachy1/HowdyPartner.py
+++ programHierachy1/HowdyPartner.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractclassmethod
 
+#Abstract class 
 class DispositivoElectronico(ABC):
     def __init__(self, marca:str) -> None:
         super().__init__()
@@ -15,11 +16,80 @@ class DispositivoElectronico(ABC):
             raise TypeError("Valor deber ser tipo str")
         self.__marca = marca
 
+    @abstractclassmethod
     def on(self):
         pass
 
+    @abstractclassmethod
     def off(self):
         pass
+    
+    def __str__(self) -> str:
+        return f"Marca: {self.marca}"
+
+# --------------------------------------------------------------------------------------------------------------------- #
 
 class Celular(DispositivoElectronico):
-    pass
+    def __init__(self, marca: str, precio: float) -> None:
+        super().__init__(marca)
+        self.precio = precio
+
+    @property
+    def precio(self):
+        return self.__precio
+    
+    @precio.setter
+    def precio(self, precio):
+        if not isinstance(precio, float):
+            raise TypeError("Valor deber ser tipo float")
+        if precio < 0:
+            raise ValueError("Precio no puede ser menor a 0")
+        self.__precio = precio
+
+    def on(self):
+        print("Prendiendo...")
+
+    def off(self):
+        print("Apagando...")
+
+    def llamar(self):
+        print("Llamando...")
+
+    def __str__(self) -> str:
+        return f"{super().__str__()}\nPrecio: {self.precio}"
+    
+
+class Television(DispositivoElectronico):
+    def __init__(self, marca: str, canales: int) -> None:
+        super().__init__(marca)
+        self.canales = canales
+
+    @property
+    def canales(self):
+        return self.__canales
+    
+    @canales.setter
+    def canales(self, canales):
+        if not isinstance(canales, int):
+            raise TypeError("Valor deber ser tipo int")
+        if canales < 0:
+            raise ValueError("Los canales no pueden ser menor a 0")
+        self.__canales = canales
+
+    def on(self):
+        print("Prendiendo...")
+
+    def off(self):
+        print("Apagando...")
+
+    def verNetflix(self):
+        print("Viendo netflix")
+
+    def __str__(self) -> str:
+        return f"{super().__str__()}\Canales: {self.canales}"
+    
+
+# -----------------------------------------------------------------------------------------------------------------------#
+
+sus = Celular(marca="Iphone", precio=float(20000))
+print(sus.llamar())
